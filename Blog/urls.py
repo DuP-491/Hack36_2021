dd3905e2bc84007b530c751b25c06b9c00515750
DuP-491 <divyansh.20198117@mnnit.ac.in>
2021-04-11 03:16:37 +0530

    bug fixes

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import  PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView,home
+from .views import  PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,PostListView, CommentCreateView,home
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
