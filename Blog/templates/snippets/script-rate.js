55b3483bea76b6a8581963094223b9abb6b5e7cd
siddheshpatil777 <siddheshpatil777@gmail.com>
2021-04-10 12:33:06 +0400

    rating Fixed

--- Blog/templates/snippets/script-rate.js
+++ Blog/templates/snippets/script-rate.js
@@ -1,59 +1,61 @@
+var starColor = "orange"
 
-function rate5(){ 
-    document.getElementById("5").style.color = "orange"; 
-        document.getElementById("4").style.color = "orange";
-      document.getElementById("3").style.color = "orange";
-      document.getElementById("2").style.color = "orange"; 
-     document.getElementById("1").style.color = "orange";
-     }
-    
-   function rate4(){
-      document.getElementById("5").style.color = "#9e9e9e"; 
-        document.getElementById("4").style.color = "orange";
-      document.getElementById("3").style.color = "orange";
-      document.getElementById("2").style.color = "orange"; 
-     document.getElementById("1").style.color = "orange";
-   }
-   
-   function rate3(){
-      document.getElementById("5").style.color = "#9e9e9e"; 
-        document.getElementById("4").style.color = "#9e9e9e";
-      document.getElementById("3").style.color = "orange";
-      document.getElementById("2").style.color = "orange"; 
-     document.getElementById("1").style.color = "orange";
-   }
-   
-   function rate2(){
-      document.getElementById("5").style.color = "#9e9e9e"; 
-        document.getElementById("4").style.color = "#9e9e9e";
-      document.getElementById("3").style.color = "#9e9e9e";
-      document.getElementById("2").style.color = "orange"; 
-     document.getElementById("1").style.color = "orange";
-   }
-   
-   function rate1(){
-      document.getElementById("5").style.color = "#9e9e9e"; 
-        document.getElementById("4").style.color = "#9e9e9e";
-      document.getElementById("3").style.color = "#9e9e9e";
-      document.getElementById("2").style.color = "#9e9e9e";
-     document.getElementById("1").style.color = "orange";
-   }
-   
-   document.getElementById("5").onclick = function(){
-     rate5();
-   }
-   document.getElementById("4").onclick = function(){
-     rate4();
-   }
-   document.getElementById("3").onclick = function(){
-     rate3();
-   }
-   document.getElementById("2").onclick = function(){
-     rate2();
-   }
-   document.getElementById("1").onclick = function(){
-     rate1();
-   }
+function rate1() {
+    document.getElementById("5").style.color = "#9e9e9e";
+    document.getElementById("4").style.color = "#9e9e9e";
+    document.getElementById("3").style.color = "#9e9e9e";
+    document.getElementById("2").style.color = "#9e9e9e";
+    document.getElementById("1").style.color = starColor;
+}
+
+function rate2() {
+    document.getElementById("5").style.color = "#9e9e9e";
+    document.getElementById("4").style.color = "#9e9e9e";
+    document.getElementById("3").style.color = "#9e9e9e";
+    document.getElementById("2").style.color = starColor;
+    document.getElementById("1").style.color = starColor;
+}
+
+function rate3() {
+    document.getElementById("5").style.color = "#9e9e9e";
+    document.getElementById("4").style.color = "#9e9e9e";
+    document.getElementById("3").style.color = starColor;
+    document.getElementById("2").style.color = starColor;
+    document.getElementById("1").style.color = starColor;
+}
+
+function rate4() {
+    document.getElementById("5").style.color = "#9e9e9e";
+    document.getElementById("4").style.color = starColor;
+    document.getElementById("3").style.color = starColor;
+    document.getElementById("2").style.color = starColor;
+    document.getElementById("1").style.color = starColor;
+}
+
+function rate5() {
+    document.getElementById("5").style.color = starColor;
+    document.getElementById("4").style.color = starColor;
+    document.getElementById("3").style.color = starColor;
+    document.getElementById("2").style.color = starColor;
+    document.getElementById("1").style.color = starColor;
+}
+
+
+document.getElementById("5").onclick = function () {
+    rate5();
+}
+document.getElementById("4").onclick = function () {
+    rate4();
+}
+document.getElementById("3").onclick = function () {
+    rate3();
+}
+document.getElementById("2").onclick = function () {
+    rate2();
+}
+document.getElementById("1").onclick = function () {
+    rate1();
+}
    
    
    
